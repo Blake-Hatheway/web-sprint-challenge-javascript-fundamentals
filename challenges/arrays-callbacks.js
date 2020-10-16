acea71a64377a30a5fa784f5210450987521983f
Blake-Hatheway <crimsoncell1@gmail.com>
2020-10-16 10:26:44 -0700

    Questions 1 & 2

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,7 +21,13 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function (item) {
+  displayNames.push((name = item.animal_name),(scientific = item.scientific_name)
+  );
+  console.log(`Name: ${name}, Scientific: ${scientific}`);
+});
 console.log(displayNames);
+
 
 /* Request 2: .map()
 
@@ -29,7 +35,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames
+const lowCaseAnimalNames = zooAnimals.map(item => {
+  return item.animal_name.toLowerCase();
+})
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -37,8 +45,8 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals
-console.log(lowPopulationAnimals);
+// const lowPopulationAnimals
+// console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
